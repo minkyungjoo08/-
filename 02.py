3c39456da27d1bcf70547f64d70055e8150cb39e
minkyungjoo08 <147910494+minkyungjoo08@users.noreply.github.com>
2023-10-21 17:34:35 +0900

    Update 02.py

--- 02.py
+++ 02.py
@@ -4,7 +4,35 @@ import folium
 import numpy 
 from streamlit_folium import st_folium
 
+with tab2:
+  col1  ,col2 = st.columns([2,1)]
 
+  col1.subhead("프로그래머를 꿈꾸는 민경주입니다.")
+  col1.text('''프로그래밍에 높은 관심을 가지고 있으며,
+AI와 SW기술에 많은 흥미를 가지고 있습니다.
+      ''')
+  col1.text('''새로운 기술을 찾고 접목하여 기존의
+문제를 효과적으로 해결하는일을 좋아합니다. ''')
+
+  col1.write("""<a href="https://minkyungjoo.streamlit.app/")
+
+  col2.image("학생증")
+
+  st.divider()
+
+  col1, col2, col3 = st.columns(3)
+
+  col1.metric("Name", "KyungJoo Min")
+  col2.metric("Birth", "2008. 09. 14")
+  col3.metric("Address", "Seoul, Korea")
+
+  col1, col2, col3 = st.columns(3)
+
+  col1.text("학교: 양정중학교 재학중")
+  col2.text("E-Mail: minkyungjoo08@gmail.com")
+  col3.text("Github: github.com/minkyungjoo08")
+  
+  
 df=pd.read_csv("gj.csv")
 df1=df.drop(['내용','등록자','시기','출처'],axis=1)
 df2=df.drop(['내용','등록자','시기','출처','인물'],axis=1)
